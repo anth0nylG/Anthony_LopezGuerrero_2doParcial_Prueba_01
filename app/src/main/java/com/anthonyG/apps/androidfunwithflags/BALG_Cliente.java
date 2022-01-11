55e1595979c888903992f46daecf1a6d7656b4e7
Anthony <anthonyxm15@gmail.com>
2022-01-10 19:00:13 -0500

    19h00

--- app/src/main/java/com/anthonyG/apps/androidfunwithflags/BALG_Cliente.java
+++ app/src/main/java/com/anthonyG/apps/androidfunwithflags/BALG_Cliente.java
@@ -1,4 +1,56 @@
 package com.anthonyG.apps.androidfunwithflags;
 
 public class BALG_Cliente {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public BALG_Cliente() {
+    }
+
+    public BALG_Cliente(int codigo, String nombre, String apellido, String correo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public BALG_Cliente(String nombre, String apellido, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
